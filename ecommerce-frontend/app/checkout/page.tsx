70ebafd390801f061fdc698fdcfb7f103845908f
Abel Guta <gutaabel546@gmail.com>
2025-11-02 21:31:11 +0300

    Checkout page initialized

--- ecommerce-frontend/app/checkout/page.tsx
+++ ecommerce-frontend/app/checkout/page.tsx
@@ -1,5 +1,63 @@
+"use client";
+import { Button } from "@/Components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
+import { useCartStore } from "@/store/cart-store";
 import React from "react";
 
 export default function page() {
-  return <div>Checkout</div>;
+  const { items, removeItem, addItem } = useCartStore();
+  const total = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  if (total === 0 || items.length === 0) {
+    return (
+      <div>
+        <h2>Your Cart is Empty.</h2>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Order Summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul>
+            {items.map((item, key) => (
+              <li key={key}>
+                <div>
+                  <span>{item.name}</span>
+                  <span>{(item.price * item.quantity).toFixed(2)}</span>
+                </div>
+
+                <div className="flex space-x-4 items-center mt-3">
+                  <Button
+                    onClick={() => removeItem(item.id)}
+                    variant={"outline"}
+                  >
+                    -
+                  </Button>
+                  <span className="text-lg font-semibold">{item.quantity}</span>
+                  <Button
+                    onClick={() => {
+                      addItem({
+                        ...item,
+                        quantity: 1,
+                      });
+                    }}
+                  >
+                    +
+                  </Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- ecommerce-frontend/app/products/[id]/page.tsx
+++ ecommerce-frontend/app/products/[id]/page.tsx
@@ -1,30 +1,32 @@
 import ProductDetail from "@/Components/ProductDetail";
 import React from "react";
 
-const dummyProduct = {
-  Name: "Wireless Headphones",
-  description:
-    "High-quality over-ear wireless headphones with noise cancellation.",
-  price: 120,
-  category: "Electronics",
-  stock: 25,
-  ratings: [
-    { userId: "1", rate: 5 },
-    { userId: "2", rate: 4 },
-  ],
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  image: "/product demo.webp",
-  id: "1",
-};
-
-export default async function page({
+export default async function Page({
   params,
 }: {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }) {
+  const { id } = await params;
+
+  console.log(id);
+
   // fetch using Id
+  const dummyProduct = {
+    Name: `Wireless Headphones ${id}`,
+    description:
+      "High-quality over-ear wireless headphones with noise cancellation.",
+    price: 120,
+    category: "Electronics",
+    stock: 25,
+    ratings: [
+      { userId: "1", rate: 5 },
+      { userId: "2", rate: 4 },
+    ],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    image: "/product demo.webp",
+    id,
+  };
+
   return <ProductDetail product={dummyProduct} />;
 }
